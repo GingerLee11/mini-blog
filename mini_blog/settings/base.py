4239069daf0fc9af5f529a4a277cff208dcbfe46
Scott Clements <clemencyoflee@gmail.com>
2022-06-08 14:33:17 -0700

    Change database configurations for postgres

--- mini_blog/settings/base.py
+++ mini_blog/settings/base.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'mini_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mini_blog_db',
+        'USER': 'blog_user',
+        'PASSWORD': 'blog_user_pswd',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- mini_blog/settings/local.py
+++ mini_blog/settings/local.py
@@ -1,5 +1,4 @@
 
-
 from .base import *
 
 INSTALLED_APPS += ["debug_toolbar"]
